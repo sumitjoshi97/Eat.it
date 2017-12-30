75cf2993e820fe0b7331e82f628d8db3d418f4cb
sumitjoshi97 <sumitjoshi479@gmail.com>
2017-12-30 22:00:02 +0530

    added Service section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,29 @@ import React, {Component} from 'react'
 const Home = () => {
     return (
         <div id="page">
-        <header>
-            <div className="hero">
-                <div className="heading">Nyet Restaurant</div>
-                <br/>
-                <div className="subHeading">the way to eat</div>
-            </div>
-        </header>
+            <header>
+                <div className="hero">
+                    <div className="heading">Nyet Restaurant</div>
+                    <br/>
+                    <div className="subHeading">the way to eat</div>
+                </div>
+            </header>
 
-        <section id="Service" className="Section">
-            <div id="imageServ">
-                <img src="../assets/Service.jpeg" alt="Service"/>
-            </div>
-        </section>
+            <section id="Service" className="Section">
+                <div id="imageServ">
+                    <img src="../assets/Service.jpeg" alt="Service" height="128"/>
+                </div>
+                <div id="Servpara">
+                <h3>Service to customers</h3>
+                    <p>Lorem ipsum dolor sit amet, ipsum explicari sit ea, an autem legendos 
+                    petentium est. Has velit paulo vocent ea, animal incorrupte suscipiantur vix te, 
+                    semper vituperatoribus est no. Sea fierent constituam ne. Simul vidisse repudiare
+                     has ut, cu iudico ornatus tractatos sea. Cu viderer recusabo tincidunt usu.
+                    </p>
+                </div>
+            </section>
         </div>
-        
+
     )
 }
 

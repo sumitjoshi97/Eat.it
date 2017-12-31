c81133719db890f87706ce032b7b9ad91b025a06
sumitjoshi97 <sumitjoshi479@gmail.com>
2017-12-31 23:29:18 +0530

    removed redundant dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,20 @@
 import React, {Component} from 'react';
-// import {BrowserRouter as Router, Link} from 'react-router-dom';
-// import logo from './logo.svg'; import './App.css';
+
 import Nav from './Nav';
 import Main from './Main';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-
-        {/* navbar and router component */}
-        <Nav/>
-        <Main/>
-
-      </div>
-
-    );
-  }
+    render() {
+        return (
+            <div>
+                {/* navbar and router component */}
+                <Nav/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
